046beaef550d08ed83e3823563a4b337cc2809e0
chanhong-ji <jichanhong24@gmail.com>
2023-11-19 15:38:04 +0900

    feat(graphql, typeorm): integrate GraphQL module & Typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import * as Joi from 'joi';
 import configuration from './config/configuration';
 
+
 @Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
 
     ConfigModule.forRoot({
       isGlobal: true,
@@ -12,6 +25,24 @@ import configuration from './config/configuration';
       validationSchema: Joi.object({
         NODE_ENV: Joi.string().valid('dev', 'prod', 'test').default('dev'),
       }),
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get('database.host'),
+          port: configService.get('database.port'),
+          username: configService.get('database.username'),
+          password: configService.get('database.password'),
+          database: configService.get('database.name'),
+          autoLoadEntities: true,
+          synchronize: process.env.NODE_ENV !== 'prod',
+          logging: ['error'],
+        };
+      },
+      inject: [ConfigService],
     }),
   ],
 })
